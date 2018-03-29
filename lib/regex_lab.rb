e23acee0cd795381e0e71865ced50704238ea7d0
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-29 03:29:17 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,4 +17,6 @@ end
 
 def valid_phone_number?(phone)
 phone.scan(/\d/).length == 10 ? true : false
+#  phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
+
 end
